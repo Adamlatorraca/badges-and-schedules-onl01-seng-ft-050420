4ba9ec5dd71ac0344651c80d340bdc6606c659fc
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-06 16:01:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,8 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 
 end
+
+def batch_badge_creator(array)
+  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+end
